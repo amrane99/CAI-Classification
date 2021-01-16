0b6c0d1a3446ac694e3627668b25b439fc87232c
Ranem <aranem@gccg301.igd.fraunhofer.de>
2021-01-16 23:21:49 +0100

    Added video transformation from 25 fps to 1 fps including labels transformation into json files.

---
+++ test.py
@@ -0,0 +1,13 @@
+from train_restore_use_models.Classification_train_restore_use import Classification_initialize_and_train
+
+
+config = {'device':'cuda:4', 'nr_runs': 1, 'cross_validation': False, 
+          'val_ratio': 0.2, 'test_ratio': 0.2, 'input_shape': (1, 299, 299),
+          'resize': False, 'augmentation': 'none', 'lr': 0.001, 'batch_size': 64,
+          'number_of_tools': 8, 'nr_epochs': 300, 
+          'random_slices': True, 'nr_videos': 40, 'nr_slices': 25,
+          'weight_decay': 0.75, 'save_interval': 25, 'msg_bot': False,
+          'bot_msg_interval': 20, 'augmented': True, 'dataset': 'Cholec80'
+         }
+
+Classification_initialize_and_train(config)
