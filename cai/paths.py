80a530f6adda9ae32b1d73520eddf0671c0631de
nilcrespo <neilcrespo15@gmail.com>
2021-01-16 15:27:21 +0100

    Update paths.py

--- cai/paths.py
+++ cai/paths.py
@@ -4,11 +4,11 @@
 import os
 
 # Path where intermediate and final results are stored
-storage_path = 'storage'
+storage_path = '/Users/nildenver/Desktop/CAI_Class/Results'
 storage_data_path = os.path.join(storage_path, 'data')
 
 # Original data paths. TODO: set necessary data paths.
-original_data_paths = {'example_dataset_name': 'example_path'}
+original_data_paths = {'Cholec80': storage_data_path}
 
 # Login for Telegram Bot
-telegram_login = {'chat_id': 'TODO', 'token': 'TODO'}
+telegram_login = {'chat_id': -421262944, 'token': 1569953194:AAGX10oX64LJfxouBLGALcQDCOQITTObZDM}
