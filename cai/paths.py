cb8d98b89060a4bf0c15e773f30180cf894acf24
Ranem <aranem@gccg301.igd.fraunhofer.de>
2021-01-21 22:19:52 +0100

    Fixed typo in paths.py

--- cai/paths.py
+++ cai/paths.py
@@ -7,8 +7,8 @@ import os
 storage_path = 'storage'
 storage_data_path = os.path.join(storage_path, 'data')
 
-# Path to save model states and results within the Code base --> Needed GUI
-# No changes necessary
+# Path to save model states and results within the Code base 
+# --> Necessary for GUI, no changes needed
 model_result_path = os.path.join(os.path.abspath(os.getcwd()), 'results')
 
 # Original data paths. TODO: set necessary data paths.
