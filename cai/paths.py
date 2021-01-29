632cceab0631733bb420230f27d40cbbc4e6efbc
PacoRahn <44786754+PacoRahn@users.noreply.github.com>
2021-01-29 01:24:10 +0100

    Update paths.py
    changed data path (testing Kaggle)

--- cai/paths.py
+++ cai/paths.py
@@ -12,7 +12,7 @@ storage_data_path = os.path.join(storage_path, 'data')
 model_result_path = os.path.join(os.path.abspath(os.getcwd()), 'results')
 
 # Original data paths. TODO: set necessary data paths.
-original_data_paths = {'Cholec80': '../../input/cholec80'}
+original_data_paths = {'Cholec80': '../../input'}
 
 # Login for Telegram Bot
 telegram_login = {'chat_id': '-421262944',
