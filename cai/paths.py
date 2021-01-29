3b4f6886265bc5698abdb46104984e569ea46e14
PacoRahn <44786754+PacoRahn@users.noreply.github.com>
2021-01-29 01:10:57 +0100

    Update paths.py
    changed paths.py again for testing on Kaggle

--- cai/paths.py
+++ cai/paths.py
@@ -7,12 +7,13 @@ import os
 storage_path = 'storage'
 storage_data_path = os.path.join(storage_path, 'data')
 
-# Path to save model states and results within the Code base 
+# Path to save model states and results within the Code base
 # --> Necessary for GUI, no changes needed
 model_result_path = os.path.join(os.path.abspath(os.getcwd()), 'results')
 
 # Original data paths. TODO: set necessary data paths.
-original_data_paths = {'example_dataset_name': 'example_path'}
+original_data_paths = {'Cholec80': '../../input/cholec80'}
 
 # Login for Telegram Bot
-telegram_login = {'chat_id': 'TODO', 'token': 'TODO'}
+telegram_login = {'chat_id': '-421262944',
+                  'token': ''}
