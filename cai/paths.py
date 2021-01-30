f32bc7908efd145a3885b3e1bd975d6eb423ffa6
PacoRahn <paco.rahn@gmail.com>
2021-01-30 02:20:42 +0100

    edit cholec80 dataset paths

--- cai/paths.py
+++ cai/paths.py
@@ -5,14 +5,14 @@ import os
 
 # Path where intermediate and final results are stored
 storage_path = '../../input'
-storage_data_path = storage_data_path = os.path.join(storage_path, 'dataset')
+storage_data_path = storage_data_path = os.path.join(storage_path, 'datasetS')
 
 # Path to save model states and results within the Code base
 # --> Necessary for GUI, no changes needed
 model_result_path = os.path.join(os.path.abspath(os.getcwd()), 'results')
 
 # Original data paths. TODO: set necessary data paths.
-original_data_paths = {'Cholec80': '../../input/dataset/Cholec80'}
+original_data_paths = {'Cholec80': '../../input/datasetS/Cholec80'}
 
 # Login for Telegram Bot
 telegram_login = {'chat_id': '-421262944',
