34262278ddb27aad6344f5eeb5924e7cc3cf2598
PacoRahn <paco.rahn@gmail.com>
2021-01-30 13:06:32 +0100

    changed storage paths

--- SurgeryToolRecognition.py
+++ SurgeryToolRecognition.py
@@ -7,6 +7,8 @@ from cai.utils.update_bots.telegram_bot import TelegramBot
 from train_restore_use_models.Classification_train_restore_use import *
 
 # 2. Train the model based on command line arguments
+
+
 def train(restore, config):
     r"""Trains a model based on config and restore values."""
     if restore:
@@ -15,36 +17,41 @@ def train(restore, config):
         Classification_initialize_and_train(config)
 
 # 3. Use the model based on command line arguments for predictions
+
+
 def test(config):
     r"""Predicts values based on config values and evaluates them."""
     Classification_test(config)
 
 # 4. Use the model based on command line arguments for predictions
+
+
 def predict(config):
     r"""Predicts values based on config values."""
     Classification_predict(config)
 
-parser = argparse.ArgumentParser(description='Train a specified model for detecting tools used in'+
-                                              ' a surgery video based on Cholec80 dataset.')
+
+parser = argparse.ArgumentParser(description='Train a specified model for detecting tools used in' +
+                                 ' a surgery video based on Cholec80 dataset.')
 parser.add_argument('--model', choices=['AlexNet', 'ResNet', 'CNN'], required=True,
                     help='Specify the model you want to use for training.')
 parser.add_argument('--mode', choices=['train', 'test', 'use'], required=True,
-                    help='Specify in which mode to use the model. Either train a model or use'+
+                    help='Specify in which mode to use the model. Either train a model or use' +
                          ' it for predictions.')
 parser.add_argument('--device', action='store', type=int, nargs=1, default=4,
-                    help='Try to train the model on the GPU device with <DEVICE> ID.'+
-                         ' Valid IDs: 0, 1, ..., 7. ID -1 would mean to use a CPU.'+
+                    help='Try to train the model on the GPU device with <DEVICE> ID.' +
+                         ' Valid IDs: 0, 1, ..., 7. ID -1 would mean to use a CPU.' +
                          ' Default: GPU device with ID 4 will be used.')
 parser.add_argument('--restore', action='store_const', const=True, default=False,
-                    help='Restore last model state and continue training from there.'+
+                    help='Restore last model state and continue training from there.' +
                          ' Default: Initialize a new model and train from beginning.')
 parser.add_argument('--use_telegram_bot', action='store_const', const=True, default=False,
-                    help='Send message during training through a Telegram Bot'+
-                         ' (Token and Chat-ID need to be provided, otherwise an error occurs!).'+
+                    help='Send message during training through a Telegram Bot' +
+                         ' (Token and Chat-ID need to be provided, otherwise an error occurs!).' +
                          ' Default: No Telegram Bot will be used to send messages.')
 parser.add_argument('--try_catch_repeat', action='store', type=int, nargs=1, default=0,
-                    help='Try to train the model with a restored state, if an error occurs.'+
-                         ' Repeat only <TRY_CATCH_REPEAT> number of times.'+
+                    help='Try to train the model with a restored state, if an error occurs.' +
+                         ' Repeat only <TRY_CATCH_REPEAT> number of times.' +
                          ' Default: Do not retry to train after an error occurs.')
 
 # 5. Define configuration dict and train the model
@@ -77,14 +84,14 @@ else:
 # nr_videos and nr_sframes Cholec80 - 80x2000 -->
 # Note: Dataset will be nr_videos x nr_frames big!
 # weight decay: Cholec80 - 0.75
-config = {'device':cuda, 'nr_runs': 1, 'cross_validation': False,
+config = {'device': cuda, 'nr_runs': 1, 'cross_validation': False,
           'val_ratio': 0.125, 'test_ratio': 0.125, 'input_shape': (3, 224, 224),
-          'resize': False, 'augmentation': 'none', 'lr': 0.001, 'batch_size': 50,
-          'number_of_tools': 7, 'nr_epochs': 1,
-          'random_frames': True, 'nr_videos': 80, 'nr_frames': 2000,
+          'resize': False, 'augmentation': 'none', 'lr': 0.001, 'batch_size': 100,
+          'number_of_tools': 7, 'nr_epochs': 100,
+          'random_frames': True, 'nr_videos': 37, 'nr_frames': 2000,
           'weight_decay': 0.75, 'save_interval': 25, 'msg_bot': msg_bot,
           'bot_msg_interval': 20, 'dataset': ds, 'model': model
-         }
+          }
 
 if mode == 'train':
     # 7. Train the model until number of epochs is reached. Send every error
@@ -98,7 +105,7 @@ if mode == 'train':
                 # Break loop if training for number epochs is concluded
                 # Otherwise, a permission denied error or other errors occured
                 break
-            except: # catch *all* exceptions
+            except:  # catch *all* exceptions
                 e = traceback.format_exc()
                 print('Error occured during training: {}'.format(e))
                 if msg_bot:
@@ -127,7 +134,7 @@ if mode == 'test':
     # with Telegram Bot if desired.
     try:
         test(config)
-    except: # catch *all* exceptions
+    except:  # catch *all* exceptions
         e = traceback.format_exc()
         print('Error occured during testing: {}'.format(e))
         if msg_bot:
@@ -138,8 +145,9 @@ if mode == 'use':
     # with Telegram Bot if desired.
     try:
         predict(config)
-    except: # catch *all* exceptions
+    except:  # catch *all* exceptions
         e = traceback.format_exc()
         print('Error occured during the use of the model: {}'.format(e))
         if msg_bot:
-            bot.send_msg('Error occured during the use of the model: {}'.format(e))
+            bot.send_msg(
+                'Error occured during the use of the model: {}'.format(e))
