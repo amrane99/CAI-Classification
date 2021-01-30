0f158fab7cce7939e3499981a0ad711fad68b14c
PacoRahn <paco.rahn@gmail.com>
2021-01-30 02:58:40 +0100

    changed storage paths

--- cai/paths.py
+++ cai/paths.py
@@ -4,8 +4,8 @@
 import os
 
 # Path where intermediate and final results are stored
-storage_path = '../../input'
-storage_data_path = storage_data_path = os.path.join(storage_path, 'sdataset')
+storage_path = '../storage'
+storage_data_path = storage_data_path = os.path.join(storage_path, 'data')
 
 # Path to save model states and results within the Code base
 # --> Necessary for GUI, no changes needed
