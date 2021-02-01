4888050d0bf8a76f9de843d09914b467f8119352
nilcrespo <32440455+nilcrespo@users.noreply.github.com>
2021-02-01 20:43:44 +0100

    50 batch size

--- SurgeryToolRecognition.py
+++ SurgeryToolRecognition.py
@@ -79,7 +79,7 @@ else:
 # weight decay: Cholec80 - 0.75
 config = {'device':cuda, 'nr_runs': 1, 'cross_validation': False, 
           'val_ratio': 0.125, 'test_ratio': 0.125, 'input_shape': (3, 224, 224),
-          'resize': False, 'augmentation': 'none', 'lr': 0.005, 'batch_size': 200,
+          'resize': False, 'augmentation': 'none', 'lr': 0.005, 'batch_size': 50,
           'number_of_tools': 7, 'nr_epochs': 300,
           'random_frames': True, 'nr_videos': 3, 'nr_frames': 2000,
           'weight_decay': 0.5, 'save_interval': 25, 'msg_bot': msg_bot,
