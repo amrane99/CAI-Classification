8278cb810cd1e9d3686ee146881d3e5222b5e98e
nilcrespo <32440455+nilcrespo@users.noreply.github.com>
2021-02-02 17:09:44 +0100

    new tuning

--- SurgeryToolRecognition.py
+++ SurgeryToolRecognition.py
@@ -79,10 +79,10 @@ else:
 # weight decay: Cholec80 - 0.75
 config = {'device':cuda, 'nr_runs': 1, 'cross_validation': False, 
           'val_ratio': 0.125, 'test_ratio': 0.125, 'input_shape': (3, 224, 224),
-          'resize': False, 'augmentation': 'none', 'lr': 0.0001, 'batch_size': 32,
+          'resize': False, 'augmentation': 'none', 'lr': 0.00005, 'batch_size': 32,
           'number_of_tools': 7, 'nr_epochs': 30,
           'random_frames': True, 'nr_videos': 5, 'nr_frames': 2000,
-          'weight_decay': 0.0005, 'save_interval': 10, 'msg_bot': msg_bot,
+          'weight_decay': 0.00005, 'save_interval': 10, 'msg_bot': msg_bot,
           'bot_msg_interval': 5, 'dataset': ds, 'model': model
          }
 
