87d9fc68a22dce0962b0727bcd7a0ae7bcf4a1f9
PacoRahn <paco.rahn@gmail.com>
2021-02-02 11:50:42 +0100

    add ResNet50 and optional feature extraction

--- cai/models/classification/CNN.py
+++ cai/models/classification/CNN.py
@@ -54,12 +54,35 @@ class VGG19BN(Model):
 class ResNet(Model):
     r"""This class represents the ResNet for image classification."""
 
-    def __init__(self, num_labels):
+    def __init__(self, num_labels, feature_extraction):
         super(ResNet, self).__init__()
         self.resnet = models.resnet18(pretrained=True)
         # Use Feature Extraction instead of finetuning
-        for param in self.resnet.parameters():
-            param.requires_grad = False
+        if feature_extraction:
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+        classifier_input = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(classifier_input, num_labels)
+        self.resnet.eval()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Reshape input based on batchsize
+        yhat = self.resnet(x)
+        yhat = self.sigmoid(yhat)
+        return yhat
+
+
+class ResNet50(Model):
+    r"""This class represents the ResNet with 50 layers for image classification."""
+
+    def __init__(self, num_labels, feature_extraction):
+        super(ResNet, self).__init__()
+        self.resnet = models.resnet50(pretrained=True)
+        # Use Feature Extraction instead of finetuning
+        if feature_extraction:
+            for param in self.resnet.parameters():
+                param.requires_grad = False
         classifier_input = self.resnet.fc.in_features
         self.resnet.fc = nn.Linear(classifier_input, num_labels)
         self.resnet.eval()
