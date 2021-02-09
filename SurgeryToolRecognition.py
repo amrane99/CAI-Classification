95e948ad16feafb4bcbf3d146f2a1224dc20d1d1
nilcrespo <32440455+nilcrespo@users.noreply.github.com>
2021-02-09 14:13:21 +0100

    updated params

--- SurgeryToolRecognition.py
+++ SurgeryToolRecognition.py
@@ -78,12 +78,12 @@ else:
 # Note: Dataset will be nr_videos x nr_frames big!
 # weight decay: Cholec80 - 0.75
 config = {'device':cuda, 'nr_runs': 1, 'cross_validation': False, 
-          'val_ratio': 0.08, 'test_ratio': 0.12, 'input_shape': (3, 224, 224),
+          'val_ratio': 0.1, 'test_ratio': 0.05, 'input_shape': (3, 224, 224),
           'resize': False, 'augmentation': 'none', 'lr': 0.00005, 'batch_size': 64,
           'number_of_tools': 7, 'nr_epochs': 5,
-          'random_frames': True, 'nr_videos': 75, 'nr_frames': 2000,
-          'weight_decay': 0.00001, 'save_interval': 1, 'msg_bot': msg_bot,
-          'bot_msg_interval': 1, 'dataset': ds, 'model': model
+          'random_frames': True, 'nr_videos': 50, 'nr_frames': 2000,
+          'weight_decay': 0.00001, 'save_interval': 2, 'msg_bot': msg_bot,
+          'bot_msg_interval': 5, 'dataset': ds, 'model': model
          }
 
 if mode == 'train':
