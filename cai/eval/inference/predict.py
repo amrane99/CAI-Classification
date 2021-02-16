6e604269baaeb0a10900b61de0d44b70688f34aa
Amin Ranem <aminranem@googlemail.com>
2021-02-16 22:34:10 +0100

    Removed test folder, added plots of trained model and created new requirements.txt file.

--- cai/eval/inference/predict.py
+++ cai/eval/inference/predict.py
@@ -13,12 +13,3 @@ def softmax(output, channel_dim=1):
     r"""Softmax outputs so that the vlues add up to 1."""
     f = torch.nn.Softmax(dim=channel_dim)
     return f(output)
-
-# TODO
-def confidence(softmaxed_output, channel_dim=1):
-    r"""Returns the confidence for each voxel (the highest value along the 
-    channel dimension)."""
-    pass
-
-def ensable_prediction():
-    pass
